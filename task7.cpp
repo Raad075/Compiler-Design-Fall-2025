9358510c9400f8e990943fe4c1c68e1333596340
Raad075 <a.raad075@gmail.com>
2025-10-29 14:47:35 +0600

    task7

---
+++ task7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string getFullName(string firstName, string lastName) {
+    string fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+int main() {
+    string firstName, lastName;
+
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+
+    string fullName = getFullName(firstName, lastName);
+
+
+    cout << "Full name: " << fullName << endl;
+
+    return 0;
+}
+
